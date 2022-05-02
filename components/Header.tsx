e34421f8db019f2ce23bf8f09cdcff1aa724bdbd
donghyeonGo <donghyeon.go@fastlane.kr>
2022-05-03 00:20:12 +0900

    init
    주의 커밋 버전 중 logo 삽입 부분 - 도서대로 하면 안된다.
    npm install --save-dev @svgr/webpack 이거 설치해줘야만 svg import 정상적으로 된다.
    슬슬 짜증남.. 책을 도대체 어떻게 만든거야..

---
+++ components/Header.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import AirbnbLogoIcon from "../public/static/svg/logo/airbnb_logo.svg";
+import AirbnbLogoText from "../public/static/svg/logo/airbnb_logo_text.svg";
+import palette from "../styles/palette";
+
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 80px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 1px 12px;
+  z-index: 10;
+  .header-logo-wrapper {
+    display: flex;
+    align-items: center;
+    .header-logo {
+      margin-right: 6px;
+    }
+  }
+
+  /** 헤더 로그인 회원가입 버튼 */
+  .header-auth-buttons {
+    .header-sign-up-button {
+      height: 42px;
+      margin-right: 8px;
+      padding: 0 16px;
+      border: 0;
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-login-button {
+      height: 42px;
+      padding: 0 16px;
+      border: 0;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+      border-radius: 21px;
+      background-color: white;
+      cursor: pointer;
+      outline: none;
+      &:hover {
+        box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.12);
+      }
+    }
+  }
+
+  .modal-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    .modal-background {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.75);
+      z-index: 10;
+    }
+    .modal-contents {
+      width: 400px;
+      height: 400px;
+      background-color: white;
+      z-index: 11;
+    }
+  }
+`;
+
+const Header: React.FC = () => {
+  //* 모달 열고 닫을 boolean 값
+  const [modalOpened, setModalOpened] = useState(false);
+  return (
+    <Container>
+      <Link href="/">
+        <a className="header-logo-wrapper">
+          <AirbnbLogoIcon />
+          <AirbnbLogoText />
+        </a>
+      </Link>
+      <div className="header-auth-buttons">
+        <button
+          className="header-sign-up-button"
+          type="button"
+          onClick={() => setModalOpened(true)}
+        >
+          회원가입
+        </button>
+        <button
+          className="header-login-button"
+          type="button"
+        >
+          로그인
+        </button>
+      </div>
+      {modalOpened && (
+        <div className="modal-wrapper">
+          <div
+            className="modal-background"
+            role="presentation"
+            onClick={() => setModalOpened(false)}
+          />
+          <div className="modal-contents" />
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export default Header;
